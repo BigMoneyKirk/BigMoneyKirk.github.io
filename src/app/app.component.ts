3ee25abecb4aa4e983e67975c9e0612e1716076d
Stephen Kirkland <skirkland.job@gmail.com>
2019-09-01 15:46:16 -0400

    Got some fun animation tools in the application in the home.component section for now. Lots of practice to do with this, but it should be fun!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { trigger } from '@angular/animations';
 
 @Component({
   selector: 'messing-around-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [ trigger('divState', []) ]
 })
 export class AppComponent {
   
--- src/app/main-content/home/home.component.ts
+++ src/app/main-content/home/home.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [trigger('divState', [
+    state('normal', style({
+      backgroundColor: 'red',
+      transform: 'translateX(0)'
+    })),
+    state('highlighted', style({
+      backgroundColor: 'blue',
+      transform: 'translateX(100px)'
+    })),
+    transition('normal <=> highlighted', animate(300)),
+    // transition('highlighted => normal', animate(800))
+  ]),
+  trigger('wildState', [
+    state('normal', style({
+      backgroundColor: 'red',
+      transform: 'translateX(0) scale(1)'
+    })),
+    state('highlighted', style({
+      backgroundColor: 'blue',
+      transform: 'translateX(100px) scale(1)'
+    })),
+    state('shrunken', style({
+      backgroundColor: 'green',
+      transform: 'translateX(0) scale(0.5)'
+    })),
+    transition('normal => highlighted', animate(300)),
+    transition('highlighted => normal', animate(800)),
+    transition('shrunken <=> *', animate(500))
+  ])
+]
 })
 export class HomeComponent implements OnInit {
+
+  public state = 'normal';
+  public wildState = 'normal';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public onAnimate(){
+    this.state == 'normal'? this.state = 'highlighted': this.state = 'normal';
+    this.wildState == 'normal'? this.wildState = 'highlighted': this.wildState = 'normal';
+  }
+
+  public onShrink(){
+    this.wildState = 'shrunken';
   }
 
 }
