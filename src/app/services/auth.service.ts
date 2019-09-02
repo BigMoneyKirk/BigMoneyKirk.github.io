e6d4b667e1fc15a8fcb425f6d2ef82eb2c3b15ea
Stephen Kirkland <skirkland.job@gmail.com>
2019-09-01 20:41:45 -0400

    still not broken. worth a push

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+  public signup(email: string, password: string){
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDVw659-42k5Fp_0J6_7b7x_WAy3tW4JkQ',
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }).subscribe()
+  }
+}
