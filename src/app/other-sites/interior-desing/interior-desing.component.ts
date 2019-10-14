e37501e220f71247e92e45ff5dae1be7bf68f98b
Stephen Kirkland <skirkland.job@gmail.com>
2019-10-13 21:42:14 -0400

    Fixed up the Angular aspect of the HTML and TS

--- src/app/other-sites/interior-desing/interior-desing.component.ts
+++ src/app/other-sites/interior-desing/interior-desing.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InteriorDesingComponent implements OnInit {
 
+  public certified_img = "assets/images/interior-design/ejs-stuff/certified-architect.JPG";
+  public chair_design = "assets/images/interior-design/ejs-stuff/chair_design.jpg";
+  public city_corener_sketch_img = "assets/images/interior-design/ejs-stuff/city-corner-sketch.JPG";
+  public contrasts_img = "assets/images/interior-design/ejs-stuff/contrasts.jpg";
+  public ejs_logo = "https://fontmeme.com/permalink/191013/d1ef77ff2ea001812916feee00c10718.png";
+
   constructor() { }
 
   ngOnInit() {
