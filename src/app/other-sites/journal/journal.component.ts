55114e0fbc063123e450e757c2d38386b6b6a432
Stephen Kirkland <skirkland.job@gmail.com>
2019-10-14 22:57:01 -0400

    There are many more updates and critiques to come. I need to sketch out the design I want for the Journal table results to come back. This should be fun!

--- src/app/other-sites/journal/journal.component.ts
+++ src/app/other-sites/journal/journal.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JournalComponent implements OnInit {
 
+  public journalUrl = "https://fontmeme.com/permalink/191015/6ed769f9c99ef18d831273a181e61f9f.png";
+  public scrollBannerUrl = "assets/images/journal/scroll-banner.png";
+
   constructor() { }
 
   ngOnInit() {
