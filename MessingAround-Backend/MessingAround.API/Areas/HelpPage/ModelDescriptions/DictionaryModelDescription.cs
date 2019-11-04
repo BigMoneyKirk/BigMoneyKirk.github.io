ec54fd2c3cc47e80dbd355496464854260898dca
Stephen Kirkland <skirkland.job@gmail.com>
2019-11-03 19:51:57 -0500

    I am going to watch a tutorial on how to set up a project with an Angular frontend already in place to the same repo

--- MessingAround-Backend/MessingAround.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MessingAround.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- MessingAround-Backend/MessingAround.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MessingAround.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
