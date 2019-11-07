ee2be74074de8908ed5bd567ddacb52614c48b15
Stephen Kirkland <skirkland.job@gmail.com>
2019-11-07 18:28:17 -0500

    pictures

--- src/app/other-sites/journal/journal.component.ts
+++ src/app/other-sites/journal/journal.component.ts
@@ -33,5 +33,4 @@ export class JournalComponent implements OnInit {
   public poem(){
     return `There we were. Just the two of us. Under the stars; talking in the cold. Alone. You and me. Me and you. Together. It felt destined. I thought about you, so I decided to write about you. About the way you make me feel. The thoughts you made me have. The laughs we shared. The good times. You shared with me your secrets, although you call yourself an open book. I like you. You like me. Let's walk together. In D.C. Around the National Mall for a good cause. That's how we met. Not only were we Best Buddies, but best buddies: we just didn't know it yet, in that time. But as time grows on, so will we. Together. I feel it. You feel me. I want you. You want me too. I pray for you, Bell. I pray for you. I think of you, Bell. I think of you. You make my heart sing! Under the stars, just us two. Alone in the cold. The sun has set, but not in our hearts. Our hearts still warm with the memories; just the thoughts we had. Denver Broncos, EMT galore! You have a puppy whom you adore! Poetic genius until I die. Praise the Lord!`;
   }
-
 }
