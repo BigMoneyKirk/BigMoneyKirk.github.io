b475d526629ba6fddcebfdd05cb6935a6636d92d
Stephen Kirkland <skirkland.job@gmail.com>
2019-11-13 18:36:34 -0500

    In order to deploy to prod, I had to add the firebase variable to the environment.prod.ts file. Plus I added a new picture of Mrs. Anne, Max, and myself to the repo.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  firebaseAPIKey: 'AIzaSyDVw659-42k5Fp_0J6_7b7x_WAy3tW4JkQ'
+  firebaseAPIKey: 'AIzaSyDVw659-42k5Fp_0J6_7b7x_WAy3tW4JkQ',
+  firebase: {
+    apiKey: 'AIzaSyDVw659-42k5Fp_0J6_7b7x_WAy3tW4JkQ',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
