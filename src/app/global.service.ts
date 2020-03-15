b47726f7a5ebb817a41661ff5736e83bbe712683
Stephen Kirkland <skirkland.job@gmail.com>
2020-03-15 03:19:56 -0400

    Added a library (animate.css) and more pictures

--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalService {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   public navigate(path: string): void {
     const route = '/' + path;
-    console.log(route);
     this.router.navigate([route]);
+  }
+
+  public pikachu(){
+    return this.http.get('https://pokeapi.co/api/v2/pokemon/pikachu');
   }
 }
--- src/app/main-content/bible/bible.component.ts
+++ src/app/main-content/bible/bible.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/global.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'messing-around-bible',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BibleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    // console.log(this.http.get('https://pokeapi.co/api/v2/pokemon/pikachu').subscribe());
+  }
+
+  public pikachu(){
+    return this.http.get('https://pokeapi.co/api/v2/pokemon/pikachu');
   }
 
 }
