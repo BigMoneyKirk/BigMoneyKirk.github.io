a2027eb8a393ba0f2e78e093d481009bcf8008d3
Stephen Kirkland <skirkland.job@gmail.com>
2020-04-19 00:25:48 -0400

    this should work...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,12 +7,12 @@ export const environment = {
   firebaseAPIKey: 'AIzaSyDVw659-42k5Fp_0J6_7b7x_WAy3tW4JkQ',
   firebase: {
     apiKey: "AIzaSyDVw659-42k5Fp_0J6_7b7x_WAy3tW4JkQ",
-      authDomain: "messingaround-524ea.firebaseapp.com",
-      databaseURL: "https://messingaround-524ea.firebaseio.com",
-      projectId: "messingaround-524ea",
-      storageBucket: "messingaround-524ea.appspot.com",
-      messagingSenderId: "480028060424",
-      appId: "1:480028060424:web:a1588c67306e7c70"
+    authDomain: "messingaround-524ea.firebaseapp.com",
+    databaseURL: "https://messingaround-524ea.firebaseio.com",
+    projectId: "messingaround-524ea",
+    storageBucket: "messingaround-524ea.appspot.com",
+    messagingSenderId: "480028060424",
+    appId: "1:480028060424:web:a1588c67306e7c70"
   }
 };
 
