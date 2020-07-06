4b712a03444a52cd617097d0e97698c491a8bc0e
Stephen Kirkland <skirkland.job@gmail.com>
2020-07-06 01:39:47 -0400

    Totally just hacked the colors of the modal so that they are the correct green for the success modal and blood red for the error modal. I'm a freakin' genius!

--- src/app/main-content/modal-content/notification-modal/notification-modal.component.ts
+++ src/app/main-content/modal-content/notification-modal/notification-modal.component.ts
@@ -15,12 +15,25 @@ export class NotificationModalComponent implements OnInit {
   ngOnInit() {
     if (this.ngxSmartModalService.getModalData('SuccessModal'))
     {
+      let modalElement = document.getElementsByClassName('nsm-content') as HTMLCollectionOf<HTMLElement>;
+      modalElement[0].style.backgroundColor = "limegreen";
+      modalElement[0].style.color = "white";
+
       this.text = this.ngxSmartModalService.getModalData('SuccessModal');
     }
     else if (this.ngxSmartModalService.getModalData('ErrorModal'))
     {
+      let modalElement = document.getElementsByClassName('nsm-content') as HTMLCollectionOf<HTMLElement>;
+      modalElement[0].style.backgroundColor = "#8a0303";
+      modalElement[0].style.color = "white";
+
       this.text = this.ngxSmartModalService.getModalData('ErrorModal');
     }
+  }
+
+  ngOnDestroy(){
+    this.ngxSmartModalService.resetModalData('SuccessModal');
+    this.ngxSmartModalService.resetModalData('ErrorModal');
   }
 
 }
