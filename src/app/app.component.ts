345b9318710cb743305f3b28e2e7037a86006039
Stephen Kirkland <skirkland.job@gmail.com>
2021-02-01 22:15:05 -0500

    Added the login/logout state changes to the app.component.ts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { trigger } from '@angular/animations';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'messing-around-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [ trigger('divState', []) ]
+  animations: [trigger('divState', [])]
 })
 export class AppComponent {
+
+  constructor(private firebaseAuth: AngularFireAuth) {}
+
+  ngOnInit() {
+    this.firebaseAuth.auth.onAuthStateChanged(user => {
+      if(user){
+        console.log(`You are signed in as ${user.email}`);
+      }
+      else{
+        console.log("You currently are not signed into the application.");
+      }
+    })
+  }
 }
