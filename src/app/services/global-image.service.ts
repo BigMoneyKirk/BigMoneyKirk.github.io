390af3cd9cb4d5befa8db26de91255beafc7e98b
Stephen Kirkland <skirkland.job@gmail.com>
2021-02-04 21:40:48 -0500

    I'll have to come back and make changes to the size of the home logo.

--- src/app/services/global-image.service.ts
+++ src/app/services/global-image.service.ts
@@ -6,8 +6,9 @@ import { Injectable } from '@angular/core';
 export class GlobalImageService {
 
   public coronaGIF = "assets/images/blog/tissue_paper_on_the_low.gif";
-  public logoUrl = 'assets/images/logos/KELogo_Resize.png';
-  public logoHomeButton = 'assets/images/logos/home-logo-button-2.PNG';
+  public logoUrl = 'assets/images/logos/KEU_Logo_FullColor.png';
+  // public logoUrl = 'assets/images/logos/KELogo_Resize.png';
+  public logoHomeButton = 'assets/images/logos/KEU_Logo_FullColor_Secondary.png';
   public kaka = 'assets/images/bunnie-beaute/kaka.jpg';
   public king = 'assets/images/people/KingKirk.png';
   public moneySmiley = 'assets/images/kirkland-loan/money-smiley.gif';
